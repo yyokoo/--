c7733d78f2202e68ecb9e4d68f471515041ecb0b
yyokoo <revaldan1357@gmail.com>
2023-04-06 11:44:32 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const question = 'ゲーム市場で、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレステーション２',
+  'ニンテンドーSwitch',
+  'NintendoDS',
+];
+const correct = 'NintendoDS';
+
+console.log(document.getElementById('js-question'));
